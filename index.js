487b0e4c48cc22848076aec7f1c232c58fa28c07
IagoLast <iago.lastra@gmail.com>
2017-02-18 01:51:32 +0100

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const assert = require('assert');
 const app = require('express')();
 const api = require('./src/api.js');
 const cache = require('./src/cache.js');
+const cors = require('cors')
 const port = process.env.PORT || 8080;
 require('dotenv').load();
 
 app.set('json spaces', 2);
+app.use(cors())
 app.use('/api/v1/', cache);
 app.use('/api/v1/', api);
 app.listen(port);
