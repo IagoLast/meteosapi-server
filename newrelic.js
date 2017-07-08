45f2c40856fdab59bd7d382def420b0414f6299f
IagoLast <iago.lastra@gmail.com>
2017-07-08 13:31:32 +0200

    Add stupid config file

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['meteosapi-server'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '56fbb9cde6ffff65e5d16401c5944b7f9c846d8d',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'trace'
+  }
+}
